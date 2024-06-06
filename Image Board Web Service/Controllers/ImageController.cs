11cd19d6d91e719e5a1f8155d39af110f887d3e7
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-06 20:35:32 +0300

    Finalised the controller methods and names

---
+++ Image Board Web Service/Controllers/ImageController.cs
@@ -0,0 +1,42 @@
+﻿using Image_Board_Web_Service.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Image_Board_Web_Service.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ImageController : ControllerBase
+{
+    [HttpGet("images/{id}")]
+    [ProducesResponseType<string>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetImage(int id) // TODO
+    {
+        return Ok();
+    }
+
+    [HttpGet("images/{id}/details")]
+    [ProducesResponseType<ImageDetailsModel>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetImageDetails(int id)
+    {
+        return Ok();
+    }
+
+    [HttpPut("images/")] // TODO
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult PutImage() // TODO
+    {
+        return Ok();
+    }
+
+    [HttpDelete("images/{id}/delete")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult DeleteImage(int id)
+    {
+        return Ok();
+    }
+}
---
+++ Image Board Web Service/Controllers/ImageReviewController.cs
@@ -0,0 +1,33 @@
+﻿using Image_Board_Web_Service.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Image_Board_Web_Service.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ImageReviewController : ControllerBase
+{
+    [HttpGet("images/reviews/{id}")]
+    [ProducesResponseType<ImageReviewModel>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public void GetImageReviews(int id)
+    {
+
+    }
+
+    [HttpPost("images/reviews/{id}/rate/{score}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public void PostImageRating(int score)
+    {
+
+    }
+
+    [HttpPost("images/reviews/{id}/comment/{comment}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public void PostImageComment(int id, string comment)
+    {
+
+    }
+}
