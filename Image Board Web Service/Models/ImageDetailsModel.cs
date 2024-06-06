0f30538845e338a098d97c52a1964e2c638f9a65
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-06 20:34:41 +0300

    Added the necessary models for the web service

---
+++ Image Board Web Service/Models/ImageDetailsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Image_Board_Web_Service.Models;
+
+public record ImageDetailsModel
+{
+    public required int Id { get; init; }
+    public required string Filename { get; init; }
+    public required int Size { get; init; }
+    public required DateTime UploadDate { get; init; }
+}
---
+++ Image Board Web Service/Models/ImageReviewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Image_Board_Web_Service.Models;
+
+public record ImageReviewModel
+{
+    public required int Id { get; init; }
+    public required string Filename { get; init; }
+    public List<string> Comments { get; init; } = [];
+    public float AverageScore { get; init; }
+}
