dade0620f9a7404d7624174ec6c3a758c3a5172d
RysteQ <34135554+RysteQ@users.noreply.github.com>
2024-06-07 02:56:17 +0300

    Finished the controllers

--- Image Board Web Service/Controllers/ImageController.cs
+++ Image Board Web Service/Controllers/ImageController.cs
@@ -1,4 +1,6 @@
-﻿using Image_Board_Web_Service.Models;
+﻿using Image_Board_Web_Service.Models.ControllerModels;
+using Image_Board_Web_Service.Models.OrmModels;
+using Image_Board_Web_Service.Services.DBController;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Image_Board_Web_Service.Controllers;
@@ -7,36 +9,86 @@ namespace Image_Board_Web_Service.Controllers;
 [Route("api/v1/[controller]")]
 public class ImageController : ControllerBase
 {
-    [HttpGet("images/{id}")]
-    [ProducesResponseType<string>(StatusCodes.Status200OK)]
+    [HttpGet("{id}")]
+    [ProducesResponseType<FileResult>(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult GetImage(int id)
     {
-        return Ok();
+        using LocalDatabaseController database = new();
+
+        ImageOrm? loadedImage = database.Images.Where(selectedImage => selectedImage.Id == id).FirstOrDefault();
+            
+        if (loadedImage != null)
+        {
+            return File(loadedImage.Data, System.Net.Mime.MediaTypeNames.Application.Octet, loadedImage.Filename);
+        }
+
+        return NotFound($"There is not a file with the ID {id}");
     }
 
-    [HttpGet("images/{id}/details")]
-    [ProducesResponseType<ImageDetailsModel>(StatusCodes.Status200OK)]
+    [HttpGet("{id}/details")]
+    [ProducesResponseType<ImageDetailsDto>(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult GetImageDetails(int id)
     {
-        return Ok();
+        using LocalDatabaseController database = new();
+
+        ImageOrm? loadedImage = database.Images.Where(selectedImage => selectedImage.Id == id).FirstOrDefault();
+
+        if (loadedImage != null)
+        {
+            ImageDetailsDto imageDetails = new()
+            {
+                Id = loadedImage.Id,
+                Filename = loadedImage.Filename,
+                Size = loadedImage.Data.Length / 1024,
+                UploadDate = loadedImage.CreationDate
+            };
+
+            return Ok(imageDetails);
+        }
+
+        return NotFound($"There is not a file with the ID {id}");
     }
 
-    [HttpPut("images/")]
+    [HttpPut("")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult PutImage(FormFile file)
+    public IActionResult PutImage(IFormFile file)
     {
+        using LocalDatabaseController database = new();
+
+        MemoryStream fileData = new();
+        ImageOrm newImage = new()
+        {
+            Filename = file.FileName
+        };
+
+        file.CopyTo(fileData);
+        newImage.Data = fileData.ToArray();
+
+        database.Add(newImage);
+        database.SaveChanges();
+
         return Ok();
     }
 
-    [HttpDelete("images/{id}/delete")]
+    [HttpDelete("{id}/delete")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult DeleteImage(int id)
     {
-        return Ok();
+        using LocalDatabaseController databse = new();
+
+        ImageOrm? loadedImage = databse.Images.Where(selectedImage => selectedImage.Id == id).FirstOrDefault();
+
+        if (loadedImage != null)
+        {
+            databse.Remove(loadedImage);
+            databse.SaveChanges();
+
+            return Ok();
+        }
+
+        return Ok($"There is not a file with the ID {id}");
     }
 }
--- Image Board Web Service/Controllers/ImageReviewController.cs
+++ Image Board Web Service/Controllers/ImageReviewController.cs
@@ -1,5 +1,8 @@
-﻿using Image_Board_Web_Service.Models;
+﻿using Image_Board_Web_Service.Models.ControllerModels;
+using Image_Board_Web_Service.Models.OrmModels;
+using Image_Board_Web_Service.Services.DBController;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Image_Board_Web_Service.Controllers;
 
@@ -7,27 +10,103 @@ namespace Image_Board_Web_Service.Controllers;
 [Route("api/v1/[controller]")]
 public class ImageReviewController : ControllerBase
 {
-    [HttpGet("images/reviews/{id}")]
-    [ProducesResponseType<ImageReviewModel>(StatusCodes.Status200OK)]
+    [HttpGet("{id}/ratings")]
+    [ProducesResponseType<List<ImageRatingDto>>(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult GetImageReviews(int id)
+    public IActionResult GetImageRatings(int id)
     {
-        return Ok();
+        using LocalDatabaseController database = new();
+
+        ImageOrm? loadedImage = database.Images.Include(selectedImage => selectedImage.Ratings).Where(selectedImage => selectedImage.Id == id).FirstOrDefault();
+
+        if (loadedImage != null)
+        {
+            List<ImageRatingDto> imageRatings = [];
+
+            foreach (ImageRatingOrm imageRating in loadedImage.Ratings)
+            {
+                imageRatings.Add(imageRating.ConvertToDto());
+            }
+
+            return Ok(imageRatings);
+        }
+
+        return NotFound($"There is not a file with the ID {id}");
     }
 
-    [HttpPost("images/reviews/{id}/rate/{score}")]
+    [HttpGet("{id}/comments")]
+    [ProducesResponseType<List<ImageRatingDto>>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult GetImageComments(int id)
+    {
+        using LocalDatabaseController database = new();
+
+        ImageOrm? loadedImage = database.Images.Include(selectedImage => selectedImage.Comments).Where(selectedImage => selectedImage.Id == id).FirstOrDefault();
+
+        if (loadedImage != null)
+        {
+            List<ImageCommentDto> imageComments = [];
+
+            foreach (ImageCommentOrm comment in loadedImage.Comments)
+            {
+                imageComments.Add(comment.ConvertToDto());
+            }
+
+            return Ok(imageComments);
+        }
+
+        return NotFound($"There is not a file with the ID {id}");
+    }
+
+    [HttpPost("{id}/rate/{score}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult PostImageRating(int score)
+    public IActionResult PostImageRating(int id, int rating)
     {
-        return Ok();
+        using LocalDatabaseController database = new();
+
+        ImageOrm? loadedImage = database.Images.Where(selectedImage => selectedImage.Id == id).FirstOrDefault();
+
+        if (loadedImage != null)
+        {
+            ImageRatingOrm ratingOrm = new()
+            {
+                Rating = rating,
+                ImageModel = loadedImage
+            };
+
+            database.Add(rating);
+            database.SaveChanges();
+
+            return Ok();
+        }
+
+        return NotFound($"There is not a file with the ID {id}");
     }
 
-    [HttpPost("images/reviews/{id}/comment/{comment}")]
+    [HttpPost("{id}/comment/{comment}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public IActionResult PostImageComment(int id, string comment)
     {
-        return Ok();
+        using LocalDatabaseController database = new();
+
+        ImageOrm? loadedImage = database.Images.Where(selectedImage => selectedImage.Id == id).FirstOrDefault();
+
+        if (loadedImage != null)
+        {
+            ImageCommentOrm commentOrm = new()
+            {
+                Comment = comment,
+                ImageModel = loadedImage
+            };
+
+            database.Add(commentOrm);
+            database.SaveChanges();
+
+            return Ok();
+        }
+
+        return NotFound($"There is not a file with the ID {id}");
     }
 }
